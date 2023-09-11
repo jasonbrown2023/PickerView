a8e5c0339639c35695ac1efa849379082c49344f
Jason Brown <jasonba@smu.edu>
2023-09-10 20:29:37 -0500

    new main

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet var picker: UIPickerView!
+    
+    let data = ["Apple", "Orange", "Banana", "Grapes", "Strawberries", "Watermelon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        picker.dataSource = self
+        picker.delegate = self
     }
-
-
 }
+    
+extension ViewController: UIPickerViewDataSource {
+        
+        
+        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+        
+        
+}
+    
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+        
+}
+
+
+
 
